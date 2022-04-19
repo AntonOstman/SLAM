2de68cd8887f9a87228ec2cb6a39f05103769ffb
antos931 <antos931@student.liu.se>
2022-04-19 21:22:24 +0200

    inital commit

---
+++ src/mapconverter.cpp
@@ -0,0 +1,20 @@
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+    glfwInit();
+    window = glfwCreateWindow(800,  600, "window", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    while(!glfwWindowShouldClose(window)){
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate( );
+    return 0;
+
+
+
+
+}
