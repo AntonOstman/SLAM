5053eb6183f82a502e824c1ba2bf0a108d24ad35
antos931 <antos931@student.liu.se>
2022-05-09 17:39:22 +0200

    save

--- src/mapconverter.cpp
+++ src/mapconverter.cpp
@@ -1,13 +1,38 @@
+#include <stdint.h>
+#include <stdlib.h>
 #include <GLFW/glfw3.h>
+#include <emscripten.h>
 
 int main(void)
 {
     GLFWwindow* window;
-    glfwInit();
-    window = glfwCreateWindow(800,  600, "window", NULL, NULL);
+    if(!glfwInit()){
+        exit(1);
+    }
+    int SCREEN_WIDTH = 800;
+    int SCREEN_HEIGHT = 600;
+    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "window", NULL, NULL);
     glfwMakeContextCurrent(window);
-    while(!glfwWindowShouldClose(window)){
+
+
+    glViewport(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT);
+    
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1);
+    glMatrixMode(GL_MODELVIEW);
+
+    GLfloat lineVertices[] = {
+        200, 100, 0,
+        100, 300, 0
+    };
+
+   while(!glfwWindowShouldClose(window)){
         glClear(GL_COLOR_BUFFER_BIT);
+        glEnableClientState(GL_VERTEX_ARRAY);
+        glVertexPointer(3, GL_FLOAT, 0, lineVertices);
+        glDrawArrays(GL_LINES, 0, 2);
+        glDisableClientState(GL_VERTEX_ARRAY);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
